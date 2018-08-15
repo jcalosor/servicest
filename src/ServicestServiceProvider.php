2537bcfe111d72f193e56cea6de132a16e5bdd22
Jerric Calosor <j.calosor@gmail.com>
2018-08-15 21:55:29 +0000

    Enhancemnt: Created the base ServiceProvider [ServicestServiceProvider], Changed the namespace to [JCalosor\Servicest]

---
+++ src/ServicestServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JCalosor\Servicest;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServicestServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
